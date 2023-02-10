7ecc7ea9a695af0ff9c5a4d8d1257cccb609d5ea
SouravBandyopadhyay <souravb.1998@gmail.com>
2023-02-10 17:52:13 +0530

    style

--- frontend/src/Components/Categories.jsx
+++ frontend/src/Components/Categories.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Select, Button, Box, HStack } from "@chakra-ui/react";
+import {
+  Select,
+  Button,
+  Box,
+  HStack,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 const Categories = ({ currentCategory, categoryChangeHandler }) => {
   const { items } = useSelector((state) => state.products);
@@ -7,13 +14,13 @@ const Categories = ({ currentCategory, categoryChangeHandler }) => {
   items.map((el) => categories.push(el.category));
   const categoriesFinal = ["All Categories", ...new Set(categories)];
   return (
-    <HStack spacing={3} justifyContent="center">
+    <Stack justifyContent="center" direction="row" spacing={4} align="center">
       {categoriesFinal.map((category) => (
-        <Button key={category} onClick={() => categoryChangeHandler(category)}>
+        <Button variant='ghost' colorScheme='pink' fontSize={["xs","sm","md"]} key={category} onClick={() => categoryChangeHandler(category)}>
           {category}
         </Button>
       ))}
-    </HStack>
+    </Stack>
   );
 };
 export default Categories;
